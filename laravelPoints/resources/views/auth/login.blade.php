94c05b43057b8a6b7e7e02683e3f5003d4454021
Satit Prontepanon <st_satitza@hotmail.com>
2017-09-18 19:36:23 +0700

    config package
    config package

--- laravelPoints/resources/views/auth/login.blade.php
+++ laravelPoints/resources/views/auth/login.blade.php
@@ -5,14 +5,14 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">เข้าสู่ระบบ</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">อีเมล์</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">รหัสผ่าน</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -39,6 +39,7 @@
                             </div>
                         </div>
 
+                        <?php /*
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
@@ -48,16 +49,18 @@
                                 </div>
                             </div>
                         </div>
+                        */ ?>
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    เข้าสู่ระบบ
                                 </button>
-                                 <?php //{{ route('password.request') }} ?>
+                                 <?php /*{{ route('password.request') }} ?>
                                 <a class="btn btn-link" href="#">
                                     Forgot Your Password?
                                 </a>
+                                */ ?>
                             </div>
                         </div>
                     </form>
--- laravelPoints/resources/views/layouts/app.blade.php
+++ laravelPoints/resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
                         <?php /*{{ config('app.name', 'Laravel') }} */?>
-                        Reward Points
+                        ระบบคำนวนแต้มสมาชิก
                     </a>
                 </div>
 
@@ -44,9 +44,9 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('login') }}">เข้าสู่ระบบ</a></li>
                             <?php //{{ route('register') }} ?>
-                            <li><a href="#">Register</a></li>
+                            <li><a href="#">สร้างผู้ใช้งาน</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -58,7 +58,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            ออกจากระบบ
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
