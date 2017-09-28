807140679dcefd7f3314c1cf16d43c8893a980c0
Satit Prontepanon <st_satitza@hotmail.com>
2017-09-28 15:27:37 +0700

    commit
    commit

--- RewardsPoints/resources/views/layouts/app.blade.php
+++ RewardsPoints/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title><?php //{{ config('app.name', 'Laravel') }}?> The Founder Corp</title>
+        <title><?php //{{ config('app.name', 'Laravel') }} ?> The Founder Corp</title>
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -38,6 +38,17 @@
                         <!-- Left Side Of Navbar -->
                         <ul class="nav navbar-nav">
                             &nbsp;
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <!--li><a href="{{ route('register') }}">Register</a></li--->
+                            @else
+
                             <li class="dropdown">
                                 <a href="{{ url('customers') }}">Customers</a>
                             </li>
@@ -58,15 +69,6 @@
                                     <li><a href="#">Add User</a></li>
                                 </ul>
                             </li>
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="nav navbar-nav navbar-right">
-                            <!-- Authentication Links -->
-                            @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                            @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
