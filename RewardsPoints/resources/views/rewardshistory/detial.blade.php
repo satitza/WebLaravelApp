0a81b1d531e3e410f7f009ad37dd22f5bcdc50cc
Satit Prontepanon <st_satitza@hotmail.com>
2017-09-29 14:12:44 +0700

    commit
    commit

--- RewardsPoints/resources/views/rewardshistory/detial.blade.php
+++ RewardsPoints/resources/views/rewardshistory/detial.blade.php
@@ -52,15 +52,69 @@
         </div>
     </div>
 </div>
+
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-primary">
-                <div class="panel-heading"><center>ที่อยู่สำหรับจัดส่ง</center></div>
-                {{ $customers["billing"]["phone"] }}
-                <div class="panel-body">
+        <div class="col-md-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading"><center>ที่อยู่สำหรับจัดส่ง</center></div>
+                            <div class="panel-body">
+                                <table class="table table-striped table-hover ">
+                                    <tbody>
+                                        <tr>
+                                            <td>เบอร์โทรศัทพ์</td>
+                                            <td>{{ $customers["billing"]["phone"] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>บริษัท</td>
+                                            <td>{{ $customers["shipping"]["company"] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>บ้านเลขที่ / ถนน</td>
+                                            <td>{{ $customers["shipping"]["address_1"] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>อพาร์ทเม้นท์ </td>
+                                            <td>{{ $customers["shipping"]["address_2"] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>อำเภอ</td>
+                                            <td>{{ $customers["shipping"]["city"] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>จังหวัด</td>
+                                            <td>{{ $customers["shipping"]["state"] }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table> 
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="col-md-6">
+            <table class="table table-striped table-hover ">
+                <thead>
+                    <tr class="success">
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="card" style="width: 20rem;">
+                                <img class="card-img-top" src="{{ asset('reward_images/1506664017.jpg') }}" alt="First slide" height="150">
+                                <div class="card-body">
+                                    <p class="card-text">Anonymous Hacker</p>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>  
         </div>
     </div>
 </div>
